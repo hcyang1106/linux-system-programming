a80b7c956e938c9ccf120be7280445004c118867
howard <howard.yang@kiwigame.com>
2023-08-16 14:29:38 +0800

    init

---
+++ fopen.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+int main () {
+    FILE *fp;
+
+    fp = fopen("tmp", "r");
+    if (fp == NULL) {
+        fprintf(stderr, "fopen(): %s\n", strerror(errno));
+        exit(1);
+    }
+
+    puts("OK");
+
+    exit(0);
+}
