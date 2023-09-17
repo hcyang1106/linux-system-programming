4d9abdc5482a40df25ea9b24f73521cf0bb4f47f
howard <howard.yang@kiwigame.com>
2023-09-17 15:22:50 -0500

    add pause.c

---
+++ Signal/pause.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main() {
+    alarm(10);
+    alarm(1);
+    alarm(5); // will only wait for five seconds
+
+    while (1) {
+        pause(); // adding pause lets cpu not occupied by while(1)
+    }
+
+    exit(0);
+}
